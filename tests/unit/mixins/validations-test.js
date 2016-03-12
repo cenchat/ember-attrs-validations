580e77282df0b4df87194fbf50490b545f5ffc1c
Mikko Paderes <rmmpaderes@gmail.com>
2016-03-12 23:24:20 +0800

    Rename attrs-validations.js to validations.js

--- tests/unit/mixins/validations-test.js
+++ tests/unit/mixins/validations-test.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import AttrsValidationsMixin from 'ember-attrs-validations/mixins/attrs-validations';
+import AttrsValidationsMixin from 'ember-attrs-validations/mixins/validations';
 import { module, test } from 'qunit';
 
 import sinon from 'sinon';
